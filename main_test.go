b65666d8c78133414350efc374323cde61ded3f4
adi-xyz <123651265+adi-xyz@users.noreply.github.com>
2023-12-06 19:35:28 +0600

    Test
    Test

--- main_test.go
+++ main_test.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"testing"
+)
 
+func TestCalc(t *testing.T) {
+
+	testCase := []struct {
+		input, expected string
+	}{
+		{"1 + 2", "3"},
+		{"VI / III", "II"},
+		{"I - II", "Вывод ошибки, так как в римской системе нет отрицательных чисел."},
+		{"I + 1", "Вывод ошибки, так как используются одновременно разные системы счисления."},
+		{"1", "Вывод ошибки, так как строка не является математической операцией."},
+		{"1 + 2 + 3", "Вывод ошибки, так как формат математической операции не удовлетворяет заданию — два операнда и один оператор (+, -, /, *)."},
+	}
+
+	for _, tc := range testCase {
+
+		result := calculator(tc.input)
+
+		if result != tc.expected {
+			t.Errorf("Для %s ожидается %s, получено %s", tc.input, tc.expected, result)
+		}
+	}
+}
+
+func TestMain(m *testing.M) {
+	fmt.Println("Инициализация тестов")
+	exitCode := m.Run()
+	fmt.Println("Выполнение тестов завершено")
+	fmt.Println("Выходной код:", exitCode)
+}
+
+func SetUp() {
+	fmt.Println("Настройка перед тестом")
+}
+
+func TearDown() {
+	fmt.Println("Очистка после теста")
 }
